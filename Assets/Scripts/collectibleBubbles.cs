516f895725071145425d40a216719291920c048c
MojtabaOHz <moji.ostad@gmail.com>
2021-02-16 13:04:24 +0100

    Initial commit

---
+++ Assets/Scripts/collectibleBubbles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class collectibleBubbles : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] moneyBag;
+        [SerializeField] private float _bubbleSR = 1;
+        private float _moneyBagTimer;
+        private void Update()
+        {
+            SpawnMoneyBag();
+
+        }
+        void SpawnMoneyBag()
+        {
+            _moneyBagTimer += Time.deltaTime;
+            if (_moneyBagTimer >= _bubbleSR)
+            {
+                var bubbleObject = moneyBag[Random.Range(0, moneyBag.Length)];
+                _moneyBagTimer = 0;
+                Instantiate(bubbleObject, new Vector3(Random.Range(-10, 10), 7, Random.Range(-10, 10)), Quaternion.identity);
+            }
+        }
+
+        float RandomX(float posX)
+        {
+            return Random.Range(-20, 20);
+        }
+    }
