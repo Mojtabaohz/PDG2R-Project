c96a6c5969bd8138930c4f47cabc75e558cb64b8
MojtabaOHz <moji.ostad@gmail.com>
2021-02-28 11:06:17 +0100

    Implement basic resources and their management.

---
+++ Assets/Scripts/OptionManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+using UnityEngine;
+using UnityEngine.UI;
+using ResourceManager = UnityTemplateProjects.ResourceManager;
+
+public class OptionManager : MonoBehaviour
+{
+    public Button option;
+    [SerializeField] public int HappinessOutcome;
+
+    [SerializeField] public int EnergyOutcome;
+
+    [SerializeField] public int MoneyOutcome;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = option.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        
+    }
+
+    void TaskOnClick(){
+        Debug.Log ("You have chosen the option!");
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityTemplateProjects
+{
+    public class ResourceManager : MonoBehaviour
+    {
+        [SerializeField] private Slider happiness;
+        private int currentHappiness;
+        [SerializeField] private Slider energy;
+        [SerializeField] private Text Money;
+        
+        void Start()
+        {
+            happiness.maxValue = 100;
+            energy.maxValue = 100;
+        }
+        public void increaseSlider(int amount,Slider slider)
+        {
+            slider.value += amount;
+        }
+
+        public void setSlider(int amount,Slider slider)
+        {
+            
+        }
+        
+        
+        
+        
+        
+        
+    }
+}
